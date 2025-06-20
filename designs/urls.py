8588964ffb5a1f45dae62370d6443eb6b2843f4b
Gary Reynolds <gary@touch.asn.au>
2025-06-21 00:38:41 +1000

    Add collection and design views

---
+++ designs/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from designs import views
+
+urlpatterns = [
+    path("collections/", views.CollectionListView.as_view(), name="collection-list"),
+    path(
+        "collections/<uuid:pk>/",
+        views.CollectionDetailView.as_view(),
+        name="collection-detail",
+    ),
+    path("designs/<uuid:pk>/", views.DesignDetailView.as_view(), name="design-detail"),
+]
