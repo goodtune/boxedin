bda75690f5cd1e0935462d4af080fabebd58b863
Gary Reynolds <gary@touch.asn.au>
2025-06-21 00:38:41 +1000

    Add collection and design views

---
+++ designs/test_views.py
@@ -0,0 +1,27 @@
+from test_plus.test import TestCase
+
+from designs.factories import CollectionFactory, DesignFactory
+
+
+class CollectionViewTests(TestCase):
+    def setUp(self):
+        self.collection = CollectionFactory()
+        self.design = DesignFactory(collection=self.collection)
+
+    def test_collection_list_view(self):
+        response = self.get("collection-list")
+        self.response_200(response)
+        self.assertContains(response, self.collection.name)
+
+    def test_collection_detail_view(self):
+        response = self.get("collection-detail", pk=self.collection.pk)
+        self.response_200(response)
+        self.assertContains(response, self.collection.name)
+        self.assertContains(response, self.design.name)
+
+    def test_design_detail_view(self):
+        response = self.get("design-detail", pk=self.design.pk)
+        self.response_200(response)
+        self.assertContains(response, self.design.name)
+        self.assertContains(response, self.collection.name)
+
--- designs/views.py
+++ designs/views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+from django.views.generic import DetailView, ListView
+
+from designs.models import Collection, Design
+
+
+class CollectionListView(ListView):
+    model = Collection
+    template_name = "designs/collection_list.html"
+    context_object_name = "collections"
+
+
+class CollectionDetailView(DetailView):
+    model = Collection
+    template_name = "designs/collection_detail.html"
+    context_object_name = "collection"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["designs"] = self.object.designs.all()
+        return context
+
+
+class DesignDetailView(DetailView):
+    model = Design
+    template_name = "designs/design_detail.html"
+    context_object_name = "design"
+
