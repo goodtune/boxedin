d72372395cea8d26b4c5de3da5e2cacc7d6cabad
Gary Reynolds <gary@touch.asn.au>
2025-06-21 23:05:46 +1000

    Fix JSON form field validation

--- designs/test_form_fields.py
+++ designs/test_form_fields.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 
 from designs.factories import CollectionFactory
 from designs.models import Design
+from designs.sizes import DesignSize
 
 
 class DesignFormFieldTests(TestCase):
@@ -21,3 +22,18 @@ class DesignFormFieldTests(TestCase):
             "dimensions",
             "Select a valid choice. DesignSize(width=999, height=999) is not one of the available choices.",
         )
+
+    def test_create_design_from_form(self):
+        collection = CollectionFactory()
+        Form = modelform_factory(Design, fields=["name", "collection", "dimensions"])
+        form = Form(
+            data={
+                "name": "Valid Design",
+                "collection": collection.pk,
+                "dimensions": '{"width": 1080, "height": 1920}',
+            }
+        )
+        self.assertTrue(form.is_valid(), form.errors)
+        design = form.save()
+        self.assertEqual(design.collection, collection)
+        self.assertEqual(design.dimensions, DesignSize(1080, 1920))
